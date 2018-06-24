30dcfa5a4292df155ac2120884d680521ef48989
Ildar Bikmamatov <vistoyn@gmail.com>
2018-06-24 09:23:07 +0000

    add 0.3.3

---
+++ bak/0.3.3/BayrellLang/OpCodes/OpValueString.js
@@ -0,0 +1,66 @@
+"use strict;"
+/*!
+ *  Bayrell Common Languages Transcompiler
+ *
+ *  (c) Copyright 2016-2018 "Ildar Bikmamatov" <support@bayrell.org>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.bayrell.org/licenses/APACHE-LICENSE-2.0.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+var rtl = require('BayrellRtl').Lib.rtl;
+var Map = require('BayrellRtl').Types.Map;
+var Vector = require('BayrellRtl').Types.Vector;
+var BaseOpCode = require('./BaseOpCode.js');
+class OpValueString extends BaseOpCode{
+	_init(){
+		super._init();
+		this.op = "op_value";
+		this.value = "";
+	}
+	assignValue(variable_name, value){
+		if (variable_name == "op") this.op = value;
+		else if (variable_name == "value") this.value = value;
+		else super.assignValue(variable_name, value);
+	}
+	takeValue(variable_name, default_value){
+		if (default_value == undefined) default_value = null;
+		if (variable_name == "op") return this.op;
+		else if (variable_name == "value") return this.value;
+		return super.takeValue(variable_name, default_value);
+	}
+	getVariablesNames(names){
+		names.push("op");
+		names.push("value");
+	}
+	/**
+	 * Returns classname of the object
+	 * @return string
+	 */
+	getClassName(){
+		return "BayrellLang.OpCodes.OpValueString";
+	}
+	/**
+	 * Constructor
+	 */
+	constructor(value){
+		if (value == undefined) value="";
+		super();
+		this.value = value;
+	}
+	/**
+	 * Destructor
+	 */
+	destructor(){
+		super.destructor();
+	}
+}
+module.exports = OpValueString;
